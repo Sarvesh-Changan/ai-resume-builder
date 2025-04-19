f0e778f8181cdfa5ddf9981d7e821d9688a1089b
Sarvesh-Changan <changansarvesh0@gmail.com>
2025-04-19 09:39:56 +0530

    Update Header.jsx

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
     const { user, isSignedIn } = useUser();
     return (
         <div className='p-3 px-5 flex justify-between shadow-md'>
-             <Link to={'/'}>
+             <Link to={'/https://ai-resume-builder-h44c87whk-sarveshs-projects-50718d97.vercel.app/'}>
             <img src='/jobLogo.png' className='cursor-pointer' width={100} height={100} />
             </Link>
             {isSignedIn ?
